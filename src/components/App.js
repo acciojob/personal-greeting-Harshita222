6a058bcd3e72cfcb0fcd2d40181ba5c52e03804c
Harshita222 <kumariharsita501@gmail.com>
2024-06-20 12:05:15 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      Enter your name:
+      <input type="text" name="name" value={name} onChange={handleChange} />
+      <h2>Hello, {name}!</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
