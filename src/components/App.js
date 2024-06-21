7a7ba93078771edb7fc7e6adb1e9f7b9a5500f31
Harshita222 <kumariharsita501@gmail.com>
2024-06-21 12:13:21 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
-import React, { useState } from "react";
-import "./../styles/App.css";
+// import React, { useState } from "react";
+// import "./../styles/App.css";
 
+// const App = () => {
+//   const [name, setName] = useState("");
+
+//   const handleChange = (event) => {
+//     setName(event.target.value);
+//   };
+//   return (
+//     <div>
+//       {/* Do not remove the main div */}
+//       Enter your name:
+//       <input type="text" name="name" value={name} onChange={handleChange} />
+//       <p>Hello, {name}!</p>
+//     </div>
+//   );
+// };
+
+// export default App;
+import React from "react";
+import "./../styles/App.css";
+import { useState } from "react";
 const App = () => {
   const [name, setName] = useState("");
-
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
   return (
     <div>
-      {/* Do not remove the main div */}
-      Enter your name:
-      <input type="text" name="name" value={name} onChange={handleChange} />
-      <p>Hello, {name}!</p>
+      <input
+        type="text"
+        placeholder="Enter your name:"
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <p>{name ? `Hello ${name}!` : ""}</p>
     </div>
   );
 };
-
 export default App;
